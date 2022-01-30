26743569c2e8295050d0af51fe573561e44941ad
Emper0rAwes0me <64761566+Emper0rAwes0me@users.noreply.github.com>
2022-01-30 12:14:03 +0000

    Update config.js

--- js/helpers/config.js
+++ js/helpers/config.js
@@ -1,8 +1,8 @@
 var EncodingEnabled = true //If true, encoding will be an option. Text will be hidden and data won't be recorded if false
-var RecordFrequency = 30//How many times per second data should try to record at. 30 is recommended.
+var RecordFrequency = 60//How many times per second data should try to record at. 30 is recommended.
 var RecordDownScale = 100 //How far down the recording should scale down numbers (0-this instead of 0-255). You can increase this past 255 for more accuracy.
 var IndluceRecordMetadata = true //If true, metadata such as the RecordFrequency and RecordDownScale are included in the file downloaded.
-var IndluceFileMetadata = true //If true, metadata such as the song and artist name are included in the file downloaded.
+var IndluceFileMetadata = false //If true, metadata such as the song and artist name are included in the file downloaded.
 var EncodeRawData = false //If true, data is encoded raw (untransformed in analysis.js)
 var EncodeEnabledByDefault = false//If true, encoding is enabled by default (no need to press e). More for personal use.
 
