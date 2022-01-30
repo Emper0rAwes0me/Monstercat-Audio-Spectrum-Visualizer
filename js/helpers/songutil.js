572a6201d40b49beaf7194e41eddb4f472e4516f
Emper0rAwes0me <64761566+Emper0rAwes0me@users.noreply.github.com>
2022-01-30 20:14:16 +0000

    Update songutil.js

--- js/helpers/songutil.js
+++ js/helpers/songutil.js
@@ -62,6 +62,7 @@ function createGist(name,desc,data,public) {
         }
     }
 };
+}
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "https://api.github.com/gists", true);
     xhr.setRequestHeader('Content-Type', 'application/vnd.github.v3+json');
