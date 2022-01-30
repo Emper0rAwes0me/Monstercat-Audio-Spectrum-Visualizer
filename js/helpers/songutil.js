ed3defe273f25709799f01a40173957ef032d3c3
Emper0rAwes0me <64761566+Emper0rAwes0me@users.noreply.github.com>
2022-01-30 16:23:00 +0000

    Update songutil.js

--- js/helpers/songutil.js
+++ js/helpers/songutil.js
@@ -357,6 +357,7 @@ function ForceStop() {
   Paused = false
   CurrentTimeOffset = 0
   if (DownloadSongData == true) {
+    DownloadSongData = false
     var ModuleName = ArtistName + "_" + SongName
     //CompiledSongData  = '<roblox xmlns:xmime="http://www.w3.org/2005/05/xmlmime" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://www.roblox.com/roblox.xsd" version="4"> <External>null</External> <External>nil</External> <Item class="ModuleScript" referent="RBX040E8D154ACF48B48C3F54832CED08C8"><Properties> <Content name="LinkedSource"><null></null></Content> <string name="Name">' + ModuleName + '</string> <ProtectedString name="Source"><![CDATA[' + CompiledSongData;
     //CompiledSongData  = CompiledSongData  + "\n}";
